67cb3edf734460479f11e204ea05394b3ae28896
Idel <idlch.perte@gmail.com>
2023-02-17 18:54:27 -0600

    Initial Commit

---
+++ Ejercicio4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio4
+//
+//  Created by De la Cruz Hernandez on 17/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
